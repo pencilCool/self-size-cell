034742368d9dbf385b4e97e19a1d02ae47fef78d
pencilCool <yhtang_xt@163.com>
2017-02-22 11:27:20 +0800

    Initial Commit

---
+++ json/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  json
+//
+//  Created by tangyuhua on 2017/2/22.
+//  Copyright © 2017年 tangyuhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ json/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  json
+//
+//  Created by tangyuhua on 2017/2/22.
+//  Copyright © 2017年 tangyuhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
